7d37b778a762faf5280f1878082f81b7a1606a5d
Evan <evantatkinson@gmail.com>
2018-01-04 19:26:43 -0800

    dummy bee data exists in database

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -11,15 +11,62 @@ db.once('open', function() {
   console.log('mongoose connected successfully');
 });
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
+
+var QuizSchema = mongoose.Schema( {
+  Subject: String,
+  Questions: Array,
+  Answers: Array,
+
+} )
+
+var Quiz = mongoose.model('Item', QuizSchema);
+
+//-----------------
+
+
+let BeeQuiz = {
+  Subject: 'Bees! bzzzzzzz',
+  Questions: ['How many bees are there in the world?',
+              'Are honeybees going extinct?',
+              'How many times can honeybees sting you?',
+              'Wasps are ...',
+              'How are bees from a hive related to each other?',
+              'What is Honey made from?',
+              'how many bees are in a hive?'
+              ],
+  Answers: [{1:'one million', 2:'13.5 billion',3:'one trillion', 4: 'more thant those', 'answer': 4},
+            {1:'one million', 2:'13.5 billion',3:'one trillion', 4: 'more thant those', 'answer': 4},
+            {1:'one million', 2:'13.5 billion',3:'one trillion', 4: 'more thant those', 'answer': 4},
+            {1:'one million', 2:'13.5 billion',3:'one trillion', 4: 'more thant those', 'answer': 4},
+            {1:'one million', 2:'13.5 billion',3:'one trillion', 4: 'more thant those', 'answer': 4},
+            {1:'one million', 2:'13.5 billion',3:'one trillion', 4: 'more thant those', 'answer': 4}
+            ]
+}
+
+var insertDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany(BeeQuiz, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+var BeeData = new Quiz(BeeQuiz);
+
+BeeData.save(function (err, BeeData) {
+  if (err) return console.error(err);
 });
 
-var Item = mongoose.model('Item', itemSchema);
+
+//-----------
 
 var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
+  Quiz.find({}, function(err, items) {
     if(err) {
       callback(err, null);
     } else {
